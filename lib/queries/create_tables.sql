c924d90ea6a2a52d0d4d9991fd5b6fea785286b3
Jason Zhou <jasonzhou@jasons-mbp.lan>
2024-06-01 15:37:40 -0700

    update

---
+++ lib/queries/create_tables.sql
@@ -0,0 +1,26 @@
+create table songs (
+	id int primary key,
+	artist_id int not null,
+	name_kana varchar(100) not null,
+	name_rom varchar(100) not null,
+	name_en varchar(100) not null,
+	youtube_link varchar(50),
+	spotify_link varchar(50),
+	notes tinytext
+);
+
+create table artists (
+	id int primary key,
+	name_kana varchar(50) not null,
+	name_rom varchar(50) not null
+);
+
+create table lyrics (
+	song_id int not null,
+	paragraph_num tinyint not null,
+	kana mediumtext not null,
+	rom mediumtext not null,
+	en mediumtext not null
+);
+
+create index lyric_song_id_index on lyrics (song_id);
